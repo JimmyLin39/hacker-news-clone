c8f6eaa80c766cc0b800f1d2d62592dd40cb7252
JimmyLin39 <jimmylin0309@email.com>
2020-03-27 16:27:46 -0700

    update post with hooks

--- app/components/Post.js
+++ app/components/Post.js
@@ -6,67 +6,89 @@ import PostMetaInfo from './PostMetaInfo'
 import Title from './Title'
 import Comment from './Comment'
 
-export default class Post extends React.Component {
-  state = {
+function fetchPostReducer(state, action) {
+  if (action.type === 'fetch') {
+    return {
+      ...state,
+      loadingPost: true,
+      loadingComments: true
+    }
+  } else if (action.type === 'post') {
+    return {
+      ...state,
+      post: action.post,
+      loadingPost: false
+    }
+  } else if (action.type === 'comments') {
+    return {
+      ...state,
+      comments: action.comments,
+      loadingComments: false
+    }
+  } else if (action.type === 'error') {
+    return {
+      ...state,
+      loadingPost: false,
+      loadingComments: false
+    }
+  } else {
+    throw new Error(`That action type is not supported`)
+  }
+}
+
+export default function Post({ loaction }) {
+  const { id } = queryString.parse(location.search)
+  const [state, dispatch] = React.useReducer(fetchPostReducer, {
     post: null,
     loadingPost: true,
     comments: null,
     loadingComments: true,
-    error: null,
-  }
-  componentDidMount() {
-    const { id } = queryString.parse(this.props.location.search)
+    error: null
+  })
+  const { post, loadingPost, comments, loadingComments, error } = state
+  React.useEffect(() => {
+    dispatch({ type: 'fetch' })
 
     fetchItem(id)
-      .then((post) => {
-        this.setState({ post, loadingPost: false })
-
+      .then(post => {
+        dispatch({ type: 'post', post })
         return fetchComments(post.kids || [])
       })
-      .then((comments) => this.setState({
-        comments,
-        loadingComments: false
-      }))
-      .catch(({ message }) => this.setState({
-        error: message,
-        loadingPost: false,
-        loadingComments: false
-      }))
-  }
-  render() {
-    const { post, loadingPost, comments, loadingComments, error } = this.state
+      .then(comments => dispatch({ type: 'comments', comments }))
+      .catch(({ message }) => dispatch({ type: 'error', message }))
+  }, [id])
 
-    if (error) {
-      return <p className='center-text error'>{error}</p>
-    }
-
-    return (
-      <React.Fragment>
-        {loadingPost === true
-          ? <Loading text='Fetching post' />
-          : <React.Fragment>
-              <h1 className='header'>
-                <Title url={post.url} title={post.title} id={post.id} />
-              </h1>
-              <PostMetaInfo
-                by={post.by}
-                time={post.time}
-                id={post.id}
-                descendants={post.descendants}
-              />
-              <p dangerouslySetInnerHTML={{__html: post.text}} />
-            </React.Fragment>}
-        {loadingComments === true
-          ? loadingPost === false && <Loading text='Fetching comments' />
-          : <React.Fragment>
-              {comments.map((comment) =>
-                <Comment
-                  key={comment.id}
-                  comment={comment}
-                />
-              )}
-            </React.Fragment>}
-      </React.Fragment>
-    )
+  if (error) {
+    return <p className='center-text error'>{error}</p>
   }
+
+  return (
+    <React.Fragment>
+      {loadingPost === true ? (
+        <Loading text='Fetching post' />
+      ) : (
+        <React.Fragment>
+          <h1 className='header'>
+            <Title url={post.url} title={post.title} id={post.id} />
+          </h1>
+          <PostMetaInfo
+            by={post.by}
+            time={post.time}
+            id={post.id}
+            descendants={post.descendants}
+          />
+          <p dangerouslySetInnerHTML={{ __html: post.text }} />
+        </React.Fragment>
+      )}
+      {loadingComments === true ? (
+        loadingPost === false && <Loading text='Fetching comments' />
+      ) : (
+        <React.Fragment>
+          {comments.map(comment => (
+            <Comment key={comment.id} comment={comment} />
+          ))}
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  )
 }
